f159ef9c1036d0deeaf52e1050d9d196f485f0d5
abed-naanaa <abednaanaa12@gmail.com>
2025-06-07 14:58:28 +0000

    10

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,8 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const [photo, user] = values;
+    .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
