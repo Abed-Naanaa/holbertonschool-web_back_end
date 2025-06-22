b38ad3b43c709890b6a3246898a3e7083e20f658
abed-naanaa <abednaanaa12@gmail.com>
2025-06-22 08:46:47 +0000

    9

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
+"""Module with a function returning a multiplier function."""
 
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Return a function that multiplies a float by the given multiplier.
+    """
+    def multiply(n: float) -> float:
+        """Multiply the float n by multiplier."""
+        return n * multiplier
+
+    return multiply
--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
+"""Module that annotates a function returning list of element lengths."""
 
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return a list of tuples with elements and their lengths.
+    """
+    return [(i, len(i)) for i in lst]
