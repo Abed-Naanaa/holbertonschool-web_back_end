60fc8abee63cc7ee4d2a3eda6662b9e20b733144
abed-naanaa <abednaanaa12@gmail.com>
2025-06-22 08:43:43 +0000

    1-5

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
+"""Module that defines a type-annotated function for string concatenation."""
 
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of two strings str1 and str2."""
+    return str1 + str2
