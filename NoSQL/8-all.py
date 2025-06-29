a91ace67f7a90bb0bce48fb39d4586aac31c796f
abed-naanaa <abednaanaa12@gmail.com>
2025-06-29 16:51:48 +0000

    d

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""List all documents in a collection"""
+""" List all documents in a collection """
 
 def list_all(mongo_collection):
     return list(mongo_collection.find())
